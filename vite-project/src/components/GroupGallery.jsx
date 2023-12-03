d4f2709d815f0cb83357efc02c530864b8c37690
Lyem <lyemningthou5@gmail.com>
2023-12-03 02:10:51 -0800

    Add back dummy groups

--- vite-project/src/components/GroupGallery.jsx
+++ vite-project/src/components/GroupGallery.jsx
@@ -3,9 +3,32 @@ import Group from './Group'; // Import your Group component
 import axios from "axios";
 //TODO: Add search bar and searching via filter
 const GroupGallery = ({toggle}) => {
+    let dummyGroups = [
+        {
+            "id": 1,
+            "name": "CS 61A Study Group",
+            "description": "Study group for CS 61A: The Structure and Interpretation of Computer Programs.",
+            "size": 12,
+            "rating": 4.6
+        },
+        {
+            "id": 2,
+            "name": "CS 61B Study Group",
+            "description": "Collaborative learning for CS 61B: Data Structures.",
+            "size": 10,
+            "rating": 4.7
+        },
+        {
+            "id": 3,
+            "name": "CS 70 Study Group",
+            "description": "Discussion group for CS 70: Discrete Mathematics and Probability Theory.",
+            "size": 15,
+            "rating": 4.5
+        }
+    ]
     
     // State to hold groups data (replace with dummyGroups for testing)
-    const [groups, setGroups] = useState([]);
+    const [groups, setGroups] = useState(dummyGroups);
 
     useEffect(() => {
         console.log("Fetching groups...")
